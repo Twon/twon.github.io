7e7be57f6d213cc932e9637bcd7d26b2dc183745
Antony Peacock <ant.peacock@gmail.com>
2026-03-22 10:14:07 -0400

    Fix publish again (#21)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import createMDX from '@next/mdx';
-import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'export',
   images: {
